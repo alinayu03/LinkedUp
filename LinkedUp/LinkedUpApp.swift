74e96ec78a031fd887cb78b645b46671d41736d6
ALINA YU <alinayu@college.harvard.edu>
2024-10-21 19:26:46 -0400

    Initial Commit

---
+++ LinkedUp/LinkedUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  LinkedUpApp.swift
+//  LinkedUp
+//
+//  Created by Alina Yu on 10/21/24.
+//
+
+import SwiftUI
+
+@main
+struct LinkedUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
